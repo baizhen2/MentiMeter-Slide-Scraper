21e96151185187e09293d4cb707a012870ec6f34
Baizhen <baizhen2@illinois.edu>
2022-04-09 15:55:07 -0500

    Add downloading image functionality

--- image.py
+++ image.py
@@ -1,11 +1,22 @@
+import shutil
+import requests
+
 class Image:
 
     def __init__(self, imageURL, name):
         self.imageURL = imageURL
         self.imageName = name
+
+    def cleanImageUrl(self):
+        self.imageUrl = self.imageURL.split("?auto=compress")[0]
     
-    def downloadImage(self):
-        return
-    def moveToFolder(self, folderURL):
-        return
-    
+    def downloadImage(self, dstFolder):
+        self.cleanImageUrl()
+
+        r = requests.get(self.imageURL, stream = True)
+
+        if r.status_code == 200:
+            r.raw.decode_content = True
+            
+            with open(dstFolder + self.imageName,'wb') as f:
+                shutil.copyfileobj(r.raw, f)
