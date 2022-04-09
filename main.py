3d183938e196347dcdf95848f0a731b5e9831edd
Baizhen <baizhen2@illinois.edu>
2022-04-09 14:54:38 -0500

    Add mkdir for folder containing slides

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json_parser
+import os
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36',
@@ -13,10 +14,15 @@ def main():
     jsonParser = json_parser.JsonParser(json)
     images = jsonParser.getImages()
 
+    os.makedirs(r'../menti_slides/' + jsonParser.getPresentationName())
 
 
 def getMentiURL(id):
     return "https://www.menti.com/core/vote-ids/" + id.replace(" ", "") + "/series"
 
 if __name__=="__main__":
+    newpath = r'../menti_slides' 
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+        
     main()
